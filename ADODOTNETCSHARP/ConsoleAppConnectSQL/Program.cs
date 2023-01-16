f9fecd34813a13282a91b01316a4df5b36c6973e
serega.maniak@yandex.ru <serega.maniak@yandex.ru>
2023-01-16 21:06:52 +0300

    Connection done

--- ADODOTNETCSHARP/ConsoleAppConnectSQL/Program.cs
+++ ADODOTNETCSHARP/ConsoleAppConnectSQL/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,20 @@ namespace ConsoleAppConnectSQL
     {
         static void Main(string[] args)
         {
-
+        MySqlConnection mysql = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQL"].ConnectionString);
+            mysql.Open();
+            Console.WriteLine("Open");
+            mysql.CreateCommand();
+            MySqlCommand mysqlCommand = new MySqlCommand("select count(*) FROM Books",mysql);
+            MySqlDataReader reader = mysqlCommand.ExecuteReader();
+            if (reader.Read())
+            {
+                Console.WriteLine(reader.GetValue(0));
+            }
+            Console.WriteLine("Done");
+            mysql.Close();
+            Console.WriteLine("Close");
+            Console.ReadLine(); 
         }
     }
 }
