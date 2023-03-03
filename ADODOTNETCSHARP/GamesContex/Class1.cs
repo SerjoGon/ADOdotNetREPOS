1b31a8e60038b05d1152d5e45414d8f57081d34a
serega.maniak@yandex.ru <serega.maniak@yandex.ru>
2023-03-03 18:49:01 +0300

    sdfgdsfg

---
+++ ADODOTNETCSHARP/GamesContex/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace GamesContex
+{
+    public class GameContex: DbContext
+    {
+        //DbSet<Games>
+    }
+}
---
+++ ADODOTNETCSHARP/GamesDLL/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace GamesDLL
+{
+    public class Games
+    {
+        string Gname { get; set; }
+        string Gcreator { get; set; }
+        string Ggenre { get; set; }
+        DateTime Gdate { get; set; }
+        public Games() { }
+    }
+}
